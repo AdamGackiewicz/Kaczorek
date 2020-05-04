cf26278278fc7a754267bd4e2fe346823ab357b2
Adam Gackiewicz <adam.gackiewicz@gmail.com>
2020-05-04 20:37:34 +0200

    Kurs - Budowanie Klasy

---
+++ Kaczorek.BL/Klient.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaczorek.BL
+{
+    public class Klient
+    {
+        public static int Licznik { get; set; }
+
+
+        private string _nazwisko;
+        
+        public string Nazwisko
+        {
+            get
+            {
+                //jakiś kod
+                return _nazwisko;
+            }
+            set
+            {
+                //jakiś kod
+                _nazwisko = value;
+            }
+
+        }
+
+        public string Imie { get; set; }
+
+        public String Email { get; set; }
+
+        public int KlientId { get; private set; }
+
+        public string ImieNazwisko
+        {
+            get
+            {
+                string imieNazwisko = Imie;
+                if (!string.IsNullOrWhiteSpace(Nazwisko))
+                {
+                    if (!string.IsNullOrWhiteSpace(imieNazwisko))
+                    {
+                        imieNazwisko += ", ";
+                    }
+                    imieNazwisko += Nazwisko;
+                }
+                return imieNazwisko;
+            } 
+        }
+    }
+}
---
+++ UnitTestProject1/KlientTest1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kaczorek.BL;
+
+namespace Kaczorek.BL.Test
+{
+    [TestClass]
+    public class KlientTest
+    {
+        [TestMethod]
+        public void ImieNazwiskoTest()
+        {
+            // Arange
+            Klient klient = new Klient();
+            klient.Imie = "Robert";
+            klient.Nazwisko = "Kowal";
+
+            string oczekiwana = "Robert, Kowal";
+            // Action ( działaj)
+            string aktualna = klient.ImieNazwisko;
+            
+            //Assert (potwierdź test)
+            Assert.AreEqual(oczekiwana, aktualna);
+        }
+        [TestMethod]
+        public void ImieNazwiskoImiePusteTest()
+        {
+            // Arange
+            Klient klient = new Klient();
+            klient.Nazwisko = "Kowal";
+
+            string oczekiwana = "Kowal";
+            // Action ( działaj)
+            string aktualna = klient.ImieNazwisko;
+
+            //Assert (potwierdź test)
+            Assert.AreEqual(oczekiwana, aktualna);
+        }
+        [TestMethod]
+        public void ImieNazwiskoNazwiskoPusteTest()
+        {
+            // Arange
+            Klient klient = new Klient();
+            klient.Imie = "Robert";
+            
+
+            string oczekiwana = "Robert";
+            // Action ( działaj)
+            string aktualna = klient.ImieNazwisko;
+
+            //Assert (potwierdź test)
+            Assert.AreEqual(oczekiwana, aktualna);
+        }
+        [TestMethod]
+        public void StaticTest()
+        {
+            // Arange
+            var kln1 = new Klient();
+            kln1.Imie = "Jacek";
+            Klient.Licznik += 1;
+
+            var kln2 = new Klient();
+            kln2.Imie = "Maciek";
+            Klient.Licznik += 1;
+
+            var kln3 = new Klient();
+            kln3.Imie = "Irek";
+            Klient.Licznik += 1;
+            
+
+            // Action ( działaj)
+
+
+
+            //Assert (potwierdź test)
+            Assert.AreEqual(3, Klient.Licznik);
+
+        }
+    }
+}

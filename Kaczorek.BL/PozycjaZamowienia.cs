3645925f1c1398e19630f340f07ab47e395961e4
Adam Gackiewicz <adam.gackiewicz@gmail.com>
2020-05-31 22:34:54 +0200

    Dodanie pozostałych klas

---
+++ Kaczorek.BL/PozycjaZamowienia.cs
@@ -0,0 +1,62 @@
+﻿namespace Kaczorek.BL
+{
+    public class PozycjaZamowienia
+    {
+        public PozycjaZamowienia()
+        {
+
+        }
+
+        public PozycjaZamowienia(int pozycjaZamowieniaId)
+        {
+            PozycjaZamowieniaId = pozycjaZamowieniaId;
+
+        }
+
+        //właściwości
+        public int PozycjaZamowieniaId { get;private set; }
+        public int Ilosc { get; set; }
+        public int ProduktId { get; set; }
+        public decimal? CenaZakupu { get; set; }
+
+        //Metody
+
+        /// <summary>
+        /// Sprawdza dane pozycjizamówienia
+        /// </summary>
+        /// <returns></returns>
+        public bool Zwaliduj()
+        {
+            var poprawne = true;
+
+            if (Ilosc <= 0 )
+                poprawne = false;
+            if (ProduktId <= 0)
+                poprawne = false;
+            if (CenaZakupu == 0)
+                poprawne = false;
+            return poprawne;
+        }
+
+        /// <summary>
+        /// Zapisuje element zamówienia
+        /// </summary>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            //Kod który zapiszuje definiowane zamówienie
+            return true;
+        }
+
+        /// <summary>
+        /// Pobieramy jedno element zamówienia
+        /// </summary>
+        /// <param name="pozycjaZamowieniaId"></param>
+        /// <returns></returns>
+        public PozycjaZamowienia Pobierz(int pozycjaZamowieniaId)
+        {
+            //Kod który pobiera określone zamowienie
+            return new PozycjaZamowienia();
+        }    
+    }
+}
---
+++ Kaczorek.BL/Produkt.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kaczorek.BL
+{
+    class Produkt
+    {
+        public Produkt()
+        {
+
+        }
+        public Produkt(int produktId)
+        {
+            ProduktId = produktId;
+        }
+        public  int ProduktId { get; private set; }
+        public Decimal? AktualnaCena { get; set; }
+        public string Opis { get; set; }
+        public string NazwaProduktu { get; set; }
+
+        //Metody
+
+        /// <summary>
+        /// Sprawdza dane produktu
+        /// </summary>
+         /// <returns></returns>
+        public bool Zwaliduj()
+        {
+            var poprawne = true;
+
+            if (string.IsNullOrWhiteSpace(NazwaProduktu))
+                poprawne = false;
+            if (AktualnaCena == null)
+                poprawne = false;            
+            return poprawne;
+        }
+
+        /// <summary>
+        /// Zapisuje produkt
+        /// </summary>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            //Kod który zapiszuje definiowany produkt
+            return true;
+        }
+
+        /// <summary>
+        /// Pobieramy jednen produkt
+        /// </summary>
+        /// <param name="produktId"></param>
+        /// <returns></returns>
+        public Klient Pobierz(int produktId)
+        {
+            //Kod który pobiera określony produkt
+            return new Klient();
+        }
+
+        /// <summary>
+        /// Pobieramy wszystkie produkty
+        /// </summary>
+        /// <returns></returns>
+        public List<Produkt> Pobierz()
+        {
+            //Kod króty pobierze wszystkie produkty
+            return new List<Produkt>();
+        }
+
+    }
+}
